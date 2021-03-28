84a88e626bb0cd8ac2e25c0fc11a170cac55126d
Michael Tonkin <michaeltonkin99@gmail.com>
2021-03-28 13:59:10 +0100

    Implemented input for courier nodes

--- controller/purchase.py
+++ controller/purchase.py
@@ -2,6 +2,8 @@ from flask import request, redirect
 from jinja2 import Environment, PackageLoader, select_autoescape
 import requests, json, sys
 from view import app
+import pandas as pd
+
 #from model.supplieragent import SupplierAgent
 
 @app.route('/purchase')
@@ -48,6 +50,8 @@ def make_purchase_request():
     potential_amount = 0 # we use this variable to track the total amount of stock we can get from various producers
     amount = int(amount)
 
+    #iterate through each company and put in a request for the defined product if the intelligent node states that product
+    #is sold by specified vendor.
     for company in peerlist:
         if item in company['products']:
             company_url = "{}/receive_purchase_req".format(company['node_address']+":8000")
@@ -59,7 +63,7 @@ def make_purchase_request():
                 break
     if potential_amount < amount:
         print("Could not find enough stock")
-    #do something if request can be partly fullfilled
+    #do something if request can be partly fulfilled
     #print("response = " +str(company_response.__dict__))
 
     #if request is rejected go to next peer with item we want
@@ -67,5 +71,20 @@ def make_purchase_request():
     #if no one can accept our request, apologise profusely
 
     #search for a courier who can handle the request
+
+    #here we generate a dictionary which will be used as the transport calendar
+    dates = pd.date_range(start=starting, end=ending)
+    l = []
+    for i in range(0, len(dates)):
+        l.append(False)
+    calendar = dict(zip(dates, l))
+
+
+    for courier in peerlist:
+        if "Courier" in courier['company_type']:
+            courier_url = "{}/receive_purchase_req".format(courier['node_address'] + ":8000")
+            courier_response = requests.post(courier_url, json=json_data, data=calendar)
+            if courier_response.json()['accepted']:
+                pass
 
     return redirect('/purchase')
