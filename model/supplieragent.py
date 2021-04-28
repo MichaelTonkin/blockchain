383ca33924da0c875af70a322663936b9a1ab233
Michael Tonkin <michaeltonkin99@gmail.com>
2021-04-28 14:11:54 +0100

    Fixed missing transaction on supply agent

--- model/supplieragent.py
+++ model/supplieragent.py
@@ -17,6 +17,19 @@ def load_inventory():
     return inventory
 
 
+def get_company_details():
+    with open('model/peerdata.json', 'r') as file:
+        data = json.loads(file.read())
+    return data
+
+
+def get_ia_node():
+    with open('model/ia_address.json', 'r') as file:
+            data = str(file.read())
+    return data[1 : len(data)-1]
+
+
+
 class SupplierAgent:
 
     def __init__(self, product, quantity, start_date, end_date, frequency):
@@ -43,7 +56,17 @@ class SupplierAgent:
                 response_data['stock'] = self.quantity
                 response_data['price'] = int(self.items_list[self.product][1][0]) * int(self.quantity)
                 print("accepted request")
-
+                requests.post(url="{}/new_transactions".format(get_ia_node()),
+                              json={
+                                  "company": response_data["company"],
+                                  "volume": self.quantity,
+                                  "req_status": 'Offer',
+                                  "item_type": self.product,
+                                  "starting_date": self.start_date,
+                                  "ending_date": self.end_date,
+                                  "frequency": self.frequency,
+                              },
+                              headers={'Content-type': 'application/json'})
         return json.dumps(response_data)
 
 
